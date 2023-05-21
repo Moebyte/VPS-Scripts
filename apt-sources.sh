1d795e303bec26e1932ded6b14e503af08351e53
Moebyte <47306349+Moebyte@users.noreply.github.com>
2023-05-21 22:10:11 +0800

    Create apt-sources.sh

---
+++ apt-sources.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 备份原有的 sources.list 文件
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+# 替换为 USTC 镜像源
+sed -i 's/http:\/\/deb.debian.org\/debian/https:\/\/mirrors.ustc.edu.cn\/debian/g' /etc/apt/sources.list
+sed -i 's/http:\/\/security.debian.org\/debian-security/https:\/\/mirrors.ustc.edu.cn\/debian-security/g' /etc/apt/sources.list
+
+# 更新镜像源
+apt update -y
