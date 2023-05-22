897b0d84c6189869c61939f36f40f8c4c0ff022f
Moebyte <47306349+Moebyte@users.noreply.github.com>
2023-05-22 12:03:09 +0800

    Update dns-configure.sh

--- dns-configure.sh
+++ dns-configure.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+# Check if curl is installed
+if ! command -v curl &> /dev/null; then
+    echo "curl not found, installing..."
+    apt update && apt install -y curl
+fi
+
 ipv4=$(curl -4 ipv4.ip.sb 2>/dev/null)
 ipv6=$(curl -6 ipv6.ip.sb 2>/dev/null)
 
-echo "" > /etc/resolv.conf
-
-if [ -n "$ipv4" ] && [ -n "$ipv6" ]; then
-  echo "Both IPv4 and IPv6 are available."
-  echo "nameserver 8.8.8.8" > /etc/resolv.conf
-  echo "nameserver 1.0.0.1" >> /etc/resolv.conf
-  echo "nameserver 2001:4860:4860::8888" >> /etc/resolv.conf
-  echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf
-elif [ -n "$ipv4" ]; then
-  echo "Only IPv4 is available."
-  echo "nameserver 8.8.8.8" > /etc/resolv.conf
-  echo "nameserver 1.0.0.1" >> /etc/resolv.conf
-elif [ -n "$ipv6" ]; then
-  echo "Only IPv6 is available."
-  echo "nameserver 2001:4860:4860::8888" > /etc/resolv.conf
-  echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf
+# Check if IP address is in China
+if curl -m 10 -s https://ipapi.co/json | grep -q 'China'; then
+  echo "IP address is in China."
+  if [ -n "$ipv4" ] && [ -n "$ipv6" ]; then
+    echo "Both IPv4 and IPv6 are available."
+    echo "nameserver 119.29.29.29" > /etc/resolv.conf
+    echo "nameserver 223.5.5.5" >> /etc/resolv.conf
+    echo "nameserver 2402:4e00::" >> /etc/resolv.conf
+    echo "nameserver 2400:3200::1" >> /etc/resolv.conf
+  elif [ -n "$ipv4" ]; then
+    echo "Only IPv4 is available."
+    echo "nameserver 119.29.29.29" > /etc/resolv.conf
+    echo "nameserver 223.5.5.5" >> /etc/resolv.conf
+  elif [ -n "$ipv6" ]; then
+    echo "Only IPv6 is available."
+    echo "nameserver 2402:4e00::" > /etc/resolv.conf
+    echo "nameserver 2400:3200::1" >> /etc/resolv.conf
+  else
+    echo "Neither IPv4 nor IPv6 is available."
+  fi
 else
-  echo "Neither IPv4 nor IPv6 is available."
+  echo "IP address is not in China."
+  if [ -n "$ipv4" ] && [ -n "$ipv6" ]; then
+    echo "Both IPv4 and IPv6 are available."
+    echo "nameserver 8.8.8.8" > /etc/resolv.conf
+    echo "nameserver 1.0.0.1" >> /etc/resolv.conf
+    echo "nameserver 2001:4860:4860::8888" >> /etc/resolv.conf
+    echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf
+  elif [ -n "$ipv4" ]; then
+    echo "Only IPv4 is available."
+    echo "nameserver 8.8.8.8" > /etc/resolv.conf
+    echo "nameserver 1.0.0.1" >> /etc/resolv.conf
+  elif [ -n "$ipv6" ]; then
+    echo "Only IPv6 is available."
+    echo "nameserver 2001:4860:4860::8888" > /etc/resolv.conf
+    echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf
+  else
+    echo "Neither IPv4 nor IPv6 is available."
+  fi
 fi
