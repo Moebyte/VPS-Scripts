7172cf1607963a7bcb366ca6efdabd75d1ef065c
Moebyte <47306349+Moebyte@users.noreply.github.com>
2023-05-22 17:10:04 +0800

    Create rinetd-web.sh

---
+++ rinetd-web.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# 默认参数值
+password=""
+port=""
+
+# 解析命令行参数
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -p|--password)
+        password="$2"
+        shift
+        shift
+        ;;
+        -port|--port)
+        # 验证格式是否正确
+        if [[ $2 =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]+$ ]]; then
+            port="$2"
+        else
+            echo "Invalid port format. Please use IP:PORT or :PORT format."
+            exit 1
+        fi
+        shift
+        shift
+        ;;
+        *)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+    esac
+done
+
+# 如果没有传入参数，则使用默认值
+if [[ -z $password ]]; then
+    password="password"
+fi
+if [[ -z $port ]]; then
+    port="0.0.0.0:8080"
+fi
+
+# 下载 rinetd-web 并移动至 /opt/rinetd
+curl -sSL "https://github.com/Moebyte/VPS-Scripts/raw/main/rinetd-web" -o /opt/rinetd/rinetd-web
+chmod +x /opt/rinetd/rinetd-web
+
+# 创建 systemd 服务
+cat <<EOF > /etc/systemd/system/rinetd-web.service
+[Unit]
+Description=rinetd-web
+After=syslog.target network-online.target
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/rinetd
+ExecStart=/opt/rinetd/rinetd-web -p $password -port $port
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 启用并启动服务
+systemctl daemon-reload
+systemctl enable rinetd-web
+systemctl start rinetd-web
