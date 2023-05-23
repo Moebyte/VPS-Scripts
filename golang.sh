a2796d9e488ca8b62ab1e60cb35ba2d0d6694865
Moebyte <47306349+Moebyte@users.noreply.github.com>
2023-05-23 11:04:06 +0800

    Update golang.sh

--- golang.sh
+++ golang.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 # 获取所有的Stable版本链接
-stable_versions=$(curl -s https://golang.org/dl/?mode=json | grep -E 'version|stable' | awk -F '"' '{print $4}' | tr '\n' ' ')
+stable_versions=$(curl -s https://go.dev/dl/?mode=json | grep -E 'version|stable' | awk -F '"' '{print $4}' | tr '\n' ' ')
 
 # 选择最新的版本
 latest_version=$(echo $stable_versions | tr ' ' '\n' | grep -E 'go[0-9]+\.[0-9]+\.[0-9]+' | sort -rV | head -n 1)
 
 # 获取最新版本的链接
-download_url="https://golang.org/dl/$latest_version.linux-amd64.tar.gz"
+download_url="https://go.dev/dl/$latest_version.linux-amd64.tar.gz"
 
 # 下载最新版本的安装包
-wget $download_url
+wget $download_url || { echo "下载失败"; exit 1; }
+
+# 检查下载的文件是否存在
+if [ ! -f "$latest_version.linux-amd64.tar.gz" ]; then
+  echo "下载的文件不存在"
+  exit 1
+fi
 
 # 解压安装包
-tar -C /usr/local -xzf $latest_version.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf $latest_version.linux-amd64.tar.gz
 
 # 设置环境变量
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-source ~/.bashrc
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc && source ~/.bashrc
+
+# 检查是否成功设置了环境变量
+if ! command -v go &> /dev/null; then
+  echo "环境变量设置失败"
+  exit 1
+fi
 
 # 测试安装
 go version
